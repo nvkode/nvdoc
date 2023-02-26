9f3841371664aa0227343486eab158e17b8b58af
Mykyta Melnyk <liswelus@gmail.com>
2023-02-26 21:54:00 +0100

    Added getVersion function

--- src/Nvdoc.php
+++ src/Nvdoc.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 
 namespace Nvkode\Nvdoc;
 
+use Composer\InstalledVersions;
+
 class Nvdoc
 {
 
-}
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        $rootPackage = InstalledVersions::getRootPackage();
+
+        if (array_key_exists('version', $rootPackage) === true) {
+            return $rootPackage['version'];
+        }
+
+        return '0.0.1';
+
+    }//end getVersion()
+
+
+}//end class
