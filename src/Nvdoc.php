673457320c1a9d31788e0dcf88bb7cc8b02c6ec1
Mykyta Melnyk <liswelus@gmail.com>
2023-02-27 00:20:10 +0100

    Added findFiles function

--- src/Nvdoc.php
+++ src/Nvdoc.php
@@ -5,9 +5,34 @@ declare(strict_types=1);
 namespace Nvkode\Nvdoc;
 
 use Composer\InstalledVersions;
+use Exception;
+use Symfony\Component\Finder\Finder;
 
 class Nvdoc
 {
+
+
+    /**
+     * @var string $rootDirectory
+     */
+    private string $rootDirectory;
+
+
+    /**
+     * @var Finder $finder
+     */
+    private Finder $finder;
+
+
+    /**
+     * @param string $rootDirectory
+     */
+    public function __construct(
+        string $rootDirectory
+    ) {
+        $this->rootDirectory = $rootDirectory;
+        $this->finder        = new Finder();
+    }
 
 
     /**
@@ -24,6 +49,61 @@ class Nvdoc
         return '0.0.1';
 
     }//end getVersion()
+
+    /**
+     * @param string $dir
+     *
+     * @return array
+     */
+    public function findFiles(string $dir): array
+    {
+        $files = [];
+
+        $this->finder->files()->in($dir);
+
+        if ($this->finder->hasResults() === true) {
+            $namespaces = $this->getDefinedNamespaces();
+
+            foreach ($this->finder as $file) {
+                foreach ($namespaces as $namespace => $path) {
+                    $className = sprintf(
+                        "%s\\%s",
+                        rtrim($namespace, '\\'),
+                        str_replace('/', '\\', str_replace('.php', '', $file->getRelativePathname()))
+                    );
+
+                    if (class_exists($className) === true) {
+                        $files[] = $className;
+                    }
+                }
+            }
+        }
+
+        return $files;
+
+    }//end findFiles()
+
+
+    /**
+     * @return array<string, string>
+     */
+    private function getDefinedNamespaces(): array
+    {
+        try {
+            $composerConfig = file_get_contents(sprintf('%s/composer.json', $this->rootDirectory));
+
+            if ($composerConfig !== false) {
+                $composerConfig = json_decode($composerConfig);
+
+                return (array) $composerConfig->autoload->{'psr-4'};
+            }
+        } catch (Exception) {
+            // Do nothing on error.
+        }
+
+        return [];
+
+    }//end getDefinedNamespaces()
 
 
 }//end class
---
+++ tests/NvdocTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nvkode\Nvdoc\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NvdocTest extends TestCase
+{
+
+}
