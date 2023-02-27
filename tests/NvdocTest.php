1d32a25afd530c2e33829cfcb07f863c04647b44
Mykyta Melnyk <liswelus@gmail.com>
2023-02-27 14:29:25 +0100

    PHPCS: check tests folder. Added PHPStan

--- tests/NvdocTest.php
+++ tests/NvdocTest.php
@@ -1,11 +1,31 @@
 <?php
 
+/**
+ * Test class for Nvdoc.php
+ * PHP Version >= 8.1
+ *
+ * @category Tests
+ * @package  Nvdoc
+ * @author   Mykyta Melnyk <liswelus@gmail.com>
+ * @license  MIT <https://github.com/nvkode/nvdoc/blob/development/LICENSE>
+ * @link     https://github.com/nvkode/nvdoc
+ */
+
 declare(strict_types=1);
 
 namespace Nvkode\Nvdoc\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * NvdocTest Class
+ *
+ * @category Test
+ * @package  Nvdoc
+ * @author   Mykyta Melnyk <liswelus@gmail.com>
+ * @license  MIT <https://github.com/nvkode/nvdoc/blob/development/LICENSE>
+ * @link     https://github.com/nvkode/nvdoc
+ */
 class NvdocTest extends TestCase
 {
 
